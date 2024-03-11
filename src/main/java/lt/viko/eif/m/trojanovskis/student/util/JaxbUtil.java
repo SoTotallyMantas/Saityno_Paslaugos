cce4f113f2662fa30b801dd0e22e48802fe3fa02
SoTotallyMantas <mantastrojak@gmail.com>
2024-03-11 14:02:07 +0200

    Added xsd validation to marshalling unmarshalling

--- src/main/java/lt/viko/eif/m/trojanovskis/student/util/JaxbUtil.java
+++ src/main/java/lt/viko/eif/m/trojanovskis/student/util/JaxbUtil.java
@@ -1,12 +1,16 @@
 package lt.viko.eif.m.trojanovskis.student.util;
 
 import org.jboss.jandex.ClassType;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.crypto.Data;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +46,9 @@ public class JaxbUtil
             JAXBContext jaxbContext = JAXBContext.newInstance(new Class[]{data.getClass(),Data.getClass()});
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File("JAXBObject.xsd"));
+            jaxbMarshaller.setSchema(schema);
             jaxbMarshaller.marshal( Data, new File( "JAXBObject.xml" ) );
             jaxbMarshaller.marshal( Data, System.out );
 
@@ -49,6 +56,8 @@ public class JaxbUtil
         }
         catch (JAXBException e)
         {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
             throw new RuntimeException(e);
         }
 
@@ -62,6 +71,9 @@ public class JaxbUtil
             File file = new File( "JAXBObject.xml" );
             JAXBContext jaxbContext = JAXBContext.newInstance(new Class[]{clazz.getClass(),Data.getClass()});
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File("JAXBObject.xsd"));
+             jaxbUnmarshaller.setSchema(schema);
             Data = (GenericList<T>) jaxbUnmarshaller.unmarshal( file );
             Data.getData().forEach(data->System.out.println(data));
 
@@ -70,6 +82,8 @@ public class JaxbUtil
         }
         catch (JAXBException e)
         {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
             throw new RuntimeException(e);
         }
 
